7faa569918648e468a6b13ccc20fa3da504b57a2
Huy Dang <xhuydng@gmail.com>
2024-11-03 00:09:20 -0400

    change file name youtube-comment to youtube_comment

--- model.py
+++ model.py
@@ -1,9 +1,32 @@
 # Import packages 
 import pandas as pd
+import pickle
+import zipfile
 
-# Load data
-file_path = '/Users/huydang/Desktop/STA314 (ML)/Project/detect-spam-youtube-comment/train.csv'
-data = pd.read_csv(file_path)
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer as vectorizer
 
-# Print the fiew few rows to confirm it's loaded correctly 
-print(data.head())
+from sklearn.naive_bayes import MultinomialNB
+
+from sklearn.metrics import confusion_matrix, classification_report 
+
+# Load data from CSV file
+z = zipfile.ZipFile('/Users/huydang/Desktop/STA314/Project/youtube_comments.zip')
+train_data = pd.read_csv(z.open('train.csv'))
+print train_data
+
+# Drop irrelevant columns; Keep only CONTENT and CLASS
+ntrain_data = train_data.drop(["COMMENT_ID","AUTHOR","DATE","VIDEO_NAME"], axis=1, inplace=False) 
+X = ntrain_data['CONTENT'].values # Get just the comment content 
+Y = ntrain_data['CLASS'].values # Get the labels 
+
+# Tokenization and Vectorization
+tfidf_vect = vectorizer(use_idf=True, lowercase=True) 
+X_train_tfidf = tfidf_vect.fit_transform(ntrain_data)
+print(ntrain_data_tfidf.shape)
+
+
+
+
+
+
