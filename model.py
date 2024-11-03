00d616de45f6c68b0d21127576164f0cc871f7a3
Huy Dang <xhuydng@gmail.com>
2024-11-03 00:32:46 -0400

    Trained model

--- model.py
+++ model.py
@@ -5,9 +5,7 @@ import zipfile
 
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer as vectorizer
-
 from sklearn.naive_bayes import MultinomialNB
-
 from sklearn.metrics import confusion_matrix, classification_report 
 
 # Load data from CSV file
@@ -15,24 +13,21 @@ z = zipfile.ZipFile('/Users/huydang/Desktop/STA314/Project/youtube_comments.zip'
 train_data = pd.read_csv(z.open('train.csv')) # Training data
 test_data = pd.read_csv(z.open('test.csv')) # Test data
 
-# Drop irrelevant columns; Keep only CONTENT and CLASS
-ntrain_data = train_data.drop(["COMMENT_ID","AUTHOR","DATE","VIDEO_NAME"], axis=1, inplace=False) 
-X = ntrain_data['CONTENT'].values # Get just the comment content 
-Y = ntrain_data['CLASS'].values # Get the labels 
+# Drop irrelevant columns for Testing dataset
+ntest_data = test_data.drop(["COMMENT_ID","AUTHOR","DATE","VIDEO_NAME"], axis=1, inplace=False) 
+X_test = ntest_data['CONTENT'].values # Get just the comment content 
 
 # Tokenization and Vectorization
+# First fit on training data
 tfidf_vect = vectorizer(use_idf=True, lowercase=True) 
-X_tfidf = tfidf_vect.fit_transform(X)
-print("Original dataframe shape:", ntrain_data.shape)
-print("Text content array shape:", X.shape)
-print("TF-IDF matrix shape:", X_tfidf.shape)
+X_train_tfidf = tfidf_vect.fit_transform(X_train)
 
-# Training the Model 
+# Then transform test data using the same vectorizer
+# Note: use transform() not fit_transform() for test data
+X_test_tfidf = tfidf_vect.transform(X_test)
+
+# Training the Model
 model = MultinomialNB()
-model.fit(X_tfidf, Y)
-
-
-
-
+model.fit(X_train_tfidf, Y_train)
 
 
