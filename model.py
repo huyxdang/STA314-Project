dac94a0bfcf1459fe02c7722ee1d031a87575c75
Huy Dang <xhuydng@gmail.com>
2024-11-03 00:45:24 -0400

    final touches

--- model.py
+++ model.py
@@ -6,16 +6,19 @@ import zipfile
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer as vectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import confusion_matrix, classification_report 
 
 # Load data from CSV file
 z = zipfile.ZipFile('/Users/huydang/Desktop/STA314/Project/youtube_comments.zip')
 train_data = pd.read_csv(z.open('train.csv')) # Training data
 test_data = pd.read_csv(z.open('test.csv')) # Test data
 
-# Drop irrelevant columns for Testing dataset
-ntest_data = test_data.drop(["COMMENT_ID","AUTHOR","DATE","VIDEO_NAME"], axis=1, inplace=False) 
-X_test = ntest_data['CONTENT'].values # Get just the comment content 
+# Keep only relevant training columns
+X_train = train_data['CONTENT'].values # Get just the comment content 
+Y_train = train_data['CLASS'].values # Get the labels 
+
+# Keep COMMENT_ID for test data but drop other irrelevant columns
+test_comment_ids = test_data['COMMENT_ID'].values  # Save comment IDs for submission
+X_test = test_data['CONTENT'].values # Get just the comment content
 
 # Tokenization and Vectorization
 # First fit on training data
@@ -23,11 +26,20 @@ tfidf_vect = vectorizer(use_idf=True, lowercase=True)
 X_train_tfidf = tfidf_vect.fit_transform(X_train)
 
 # Then transform test data using the same vectorizer
-# Note: use transform() not fit_transform() for test data
 X_test_tfidf = tfidf_vect.transform(X_test)
 
 # Training the Model
 model = MultinomialNB()
 model.fit(X_train_tfidf, Y_train)
 
+# Make predictions on test data
+Y_pred = model.predict(X_test_tfidf)
 
+# Create submission dataframe
+submission_df = pd.DataFrame({
+    'COMMENT_ID': test_comment_ids,
+    'CLASS': Y_pred
+})
+
+# Save submission file
+submission_df.to_csv('submission.csv', index=False)
