80d8523d15937914461fd0b9c4963af6c84c30e0
Huy Dang <xhuydng@gmail.com>
2024-11-03 00:16:50 -0400

    load test data

--- model.py
+++ model.py
@@ -11,9 +11,9 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix, classification_report 
 
 # Load data from CSV file
-z = zipfile.ZipFile('youtube_comments.zip')
-train_data = pd.read_csv(z.open('train.csv'))
-print train_data
+z = zipfile.ZipFile('/Users/huydang/Desktop/STA314/Project/youtube_comments.zip')
+train_data = pd.read_csv(z.open('train.csv')) # Training data
+test_data = pd.read_csv(z.open('test.csv')) # Test data
 
 # Drop irrelevant columns; Keep only CONTENT and CLASS
 ntrain_data = train_data.drop(["COMMENT_ID","AUTHOR","DATE","VIDEO_NAME"], axis=1, inplace=False) 
@@ -22,8 +22,14 @@ Y = ntrain_data['CLASS'].values # Get the labels
 
 # Tokenization and Vectorization
 tfidf_vect = vectorizer(use_idf=True, lowercase=True) 
-X_train_tfidf = tfidf_vect.fit_transform(ntrain_data)
-print(ntrain_data_tfidf.shape)
+X_tfidf = tfidf_vect.fit_transform(X)
+print("Original dataframe shape:", ntrain_data.shape)
+print("Text content array shape:", X.shape)
+print("TF-IDF matrix shape:", X_tfidf.shape)
+
+# Training the Model 
+model = MultinomialNB()
+model.fit(X_tfidf, Y)
 
 
 
