b04e9feb633913ba60f8499328e3393d362f75fc
huyxdang <xhuydng@gmail.com>
2024-12-07 05:14:17 -0500

    Update BoW_NaiveBayes.py

--- (1) BoW (c)/BoW_NaiveBayes.py
+++ (1) BoW (c)/BoW_NaiveBayes.py
@@ -70,8 +70,3 @@ average_metrics = metrics_df.drop(columns=["Fold"]).mean(axis=0).to_dict()
 print("\nAverage Metrics Across Folds:")
 for metric, value in average_metrics.items():
     print(f"{metric}: {value:.4f}")
-
-# Train the final model on the entire training dataset and make predictions on the test dataset
-final_model = MultinomialNB()
-final_model.fit(X_train_bow, Y_train)
-Y_test_pred = final_model.predict(X_test_bow)
